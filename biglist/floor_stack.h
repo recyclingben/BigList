91c33a386c8798a33cfcccb90afd705c2a2ae972
recyclingben <bdcubbage@gmail.com>
2020-09-10 22:57:48 -0400

    Be unstupid (For some reason I forgot branchless != no comp operators.)

--- biglist/floor_stack.h
+++ biglist/floor_stack.h
@@ -4,22 +4,9 @@
 #include <stdio.h>
 #define offset(of, by) ((void *)((char *)(of) + (int)(by)))
 
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define depth_track_hi_16(ptr) ((uint16_t *)offset(ptr, 2))
-#define depth_track_lo_16(ptr) ((uint16_t *)ptr)
-#else
-#define depth_track_hi_16(ptr) ((uint16_t *)ptr)
-#define depth_track_lo_16(ptr) ((uint16_t *)offset(ptr, 2))
-#endif
-
-typedef union {
-    uint32_t int_32;
-    uint16_t int_16;
-} uint_32_16;
-
 typedef struct {
     int content_sizeof;
-    uint_32_16 depth_track;
+    int depth_track;
 } floor_stack_Stack;
 
 static inline void floor_stack_make(int content_sizeof,
@@ -47,10 +34,7 @@ static inline void floor_stack_make(int content_sizeof,
     *out_stack = calloc(1, sizeof(floor_stack_Stack)
         + (uint16_t)~0 * content_sizeof);
     (*out_stack)->content_sizeof = content_sizeof;
-
-
-    *depth_track_hi_16(&(*out_stack)->depth_track) = 1;
-    *depth_track_lo_16(&(*out_stack)->depth_track) = 0;
+    (*out_stack)->depth_track    = 0;
 
     floor_stack_peak(*out_stack, out_floor);
 }
@@ -58,15 +42,14 @@ static inline void floor_stack_make(int content_sizeof,
 static inline void floor_stack_push(floor_stack_Stack *stack,
                                     void **out_content)
 {
-    ++stack->depth_track.int_32;
+    ++stack->depth_track;
     floor_stack_peak(stack, out_content);
 }
 
 static inline void floor_stack_peak(floor_stack_Stack *stack,
                                     void **out_content)
 {
-    *out_content = offset(stack, sizeof(floor_stack_Stack)
-        + *depth_track_lo_16(&stack->depth_track) * stack->content_sizeof);
+    *out_content = offset(stack, sizeof(floor_stack_Stack) + stack->depth_track * stack->content_sizeof);
 }
 
 static inline void floor_stack_peak_floor(floor_stack_Stack *stack,
@@ -77,18 +60,9 @@ static inline void floor_stack_peak_floor(floor_stack_Stack *stack,
 
 static inline bool floor_stack_pop_maybe(floor_stack_Stack *stack)
 {
-    bool old_lo = *depth_track_lo_16(&stack->depth_track);
-    bool new_lo;
-
-    /* Trick to decrement 16 bit integer, unless it would underflow. */
-    /* Relies on higher 16 bits being == 1. */
-    --stack->depth_track.int_32;
-    *depth_track_lo_16(&stack->depth_track) -= *depth_track_hi_16(&stack->depth_track);
-    *depth_track_lo_16(&stack->depth_track) += 1;
-    *depth_track_hi_16(&stack->depth_track) = 1;
-
-    new_lo = *depth_track_lo_16(&stack->depth_track);
-    return old_lo != new_lo;
+    bool dec = stack->depth_track != 0;
+    stack->depth_track -= dec;
+    return dec;
 }
 
 static inline void floor_stack_clear(floor_stack_Stack *stack)
